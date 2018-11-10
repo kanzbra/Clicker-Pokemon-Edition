a5b7bd9bc9c7c851b1b428b9323129f0d5ddea71
kanzbra rashid <kanzbra.rashid@gmail.com>
2018-11-10 16:45:17 -0500

    Updates

---
+++ precache-manifest.26327cd70b58d15986ae6b636ba9a9c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e77a7fa345ffd768bc93",
+    "url": "/Clicker-Pokemon-Edition/static/js/runtime~main.e77a7fa3.js"
+  },
+  {
+    "revision": "5e1df594fe6f8a974bda",
+    "url": "/Clicker-Pokemon-Edition/static/js/main.5e1df594.chunk.js"
+  },
+  {
+    "revision": "705380186d4e6d38d8b0",
+    "url": "/Clicker-Pokemon-Edition/static/js/1.70538018.chunk.js"
+  },
+  {
+    "revision": "5e1df594fe6f8a974bda",
+    "url": "/Clicker-Pokemon-Edition/static/css/main.b9dc91f5.chunk.css"
+  },
+  {
+    "revision": "5c256a4eab26884b41361ca9e70df33a",
+    "url": "/Clicker-Pokemon-Edition/index.html"
+  }
+];
